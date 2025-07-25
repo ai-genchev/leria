d29dd4508aabc55c4d35e698eb54a8ee345d8992
ai-genchev <nasko99xd@gmail.com>
2025-07-26 02:05:17 +0300

    Add images

--- app/javascript/hamburger.js
+++ app/javascript/hamburger.js
@@ -1,10 +1,14 @@
 document.addEventListener("turbo:load", () => {
   const toggle = document.getElementById("menu-toggle");
+  const close = document.getElementById("menu-close");
   const menu = document.getElementById("menu");
 
   if (!toggle || !menu) return;
 
   toggle.addEventListener("click", () => {
-    menu.style.display = menu.style.display === "flex" ? "none" : "flex";
+    menu.classList.remove("hidden");
+  });
+  close.addEventListener("click", () => {
+    menu.classList.add("hidden");
   });
 });
